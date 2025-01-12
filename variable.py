25b0bb7ac5f73ab6c8b87206760717b3aa425a8d
teaguk <138425745+teaguk@users.noreply.github.com>
2025-01-12 02:45:33 +0000

    20250112

---
+++ variable.py
@@ -0,0 +1,58 @@
+# variable
+# variable=value
+# save the value in variable in the format above.
+# = Unlike the meaning of the symbol, the value is simply stored in a variable
+# Variable can be whatever as long as you follow the rules below
+# 1. Impossible to start with a number
+# 2. special characters are not possible exept the symbol"_"
+# 3. no spacing
+# 4. reserved words not possible
+
+# my_name = "eugenia"
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+# data types
+# str   -> String     => made up of quotes" "  ' '
+# float -> float      => no quotes and has a decimal point 
+# int    ->integer    =>no quotes and has no decmal point 
+# everyday words (english, chinese, korean, etc) without quotes => error 
+# a=10    # saves the integer 10 in the variable a
+# b='cit'     # saves the string 'cit' in the variable b
+
+
+# name='python'
+# age=20
+# height=178
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+# print('text' or variable or number)
+# the print() function prints the 'text', number, or value of
+# the variable inside the bracket and then adds a newline.
+# to print multiple items, use a comma (,)
+# if you use print () with nothing inside, it prints an empty line
+
+# name='python'
+# age=10
+# height=140
+# print (name)
+# print ()        #prints an empty line
+# print(age)
+# print(height)
+# print('hi')
+# print()
+# print(5)
+# print(5*10)
+# print(name, age, height, "hello")
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+name='eugenia'
+school='YISS'
+print("hello my ame is ", name)
+print("i am currently a 5th-grade student in", school)
